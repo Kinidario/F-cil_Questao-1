3815f5d7ec51fa06ea77f72bc50a9437593d19d3
Kinidario <102186057+Kinidario@users.noreply.github.com>
2022-03-23 10:54:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+#Função
 def repetir(n, texto):
+#Criando a variável local
   vetor = []
+#Estrutura de repetição para adicionar a string no vetor
   for i in range(0,n):
     vetor.append(texto)
+ #Retornando o vetor
   return vetor
   
-
+#Corpo principal do código
+#Recebendo as variáveiis
 n = int(input("Digite um numero : "))
 texto = input("Digite algo : ")
-
+#A variável palavras receberá o vetor retornado na função
 palavras = repetir(n, texto)
 
 print(palavras)
